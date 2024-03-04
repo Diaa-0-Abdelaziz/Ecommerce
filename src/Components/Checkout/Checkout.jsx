286304284bb978ee5a5a408e29d114ca11f515a8
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-03-04 18:39:11 +0200

    modified:   src/Components/Checkout/Checkout.jsx

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -39,7 +39,7 @@ export default function Checkout() {
   })
   async function paymentHandeling(shippingAddress){
    let endPoint =  isOnline?
-      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=http://localhost:3000`
+      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://ecommerce-six-sage.vercel.app/`
     :
      `https://ecommerce.routemisr.com/api/v1/orders/${cartId}`
     
